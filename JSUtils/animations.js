c902bb513ab43e2bc3580e3131a8130810c12166
armancoding <arman.armament@gmail.com>
2022-12-07 09:07:11 -0500

    First Commit

---
+++ JSUtils/animations.js
@@ -0,0 +1,14 @@
+let tabButtons = document.querySelectorAll(".headMenu button");
+let panels = document.querySelectorAll(".tabs")
+
+function showPanel(panelIndex)
+{
+    panels.forEach(function(node)
+    {
+        node.style.display="none";
+    });
+    panels[panelIndex].style.display="block";
+
+}
+
+showPanel(0);
