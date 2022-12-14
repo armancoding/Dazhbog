034fcdd064187d1756a5a9c6252748b997a70cd4
armancoding <arman.armament@gmail.com>
2022-12-14 00:32:31 -0500

    hamburger icon added

--- JSUtils/animations.js
+++ JSUtils/animations.js
@@ -13,3 +13,55 @@ function showPanel(panelIndex)
 
 showPanel(0);
 
+let coll = document.getElementsByClassName("collapsible");
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+let icon = document.getElementById("icon");
+let Logo = document.getElementById("Logo")
+let icon1 = document.getElementById("a");
+let icon2 = document.getElementById("b");
+let icon3 = document.getElementById("c");
+let nav = document.getElementById('nav');
+let blue = document.getElementById("blue");
+
+if(window.innerWidth > 500)
+{
+    icon.style.display="none";
+    Logo.style.display="none";
+}
+
+window.addEventListener('resize', function() 
+{
+    if(window.innerWidth > 500)
+    {
+        icon.style.display="none";
+        Logo.style.display="none";
+
+    }
+    else
+    {
+        icon.style.display="inline";
+        Logo.style.display="inline";
+    }
+});
+
+icon.addEventListener('click', function() 
+{
+    icon1.classList.toggle('a');
+    icon2.classList.toggle('c');
+    icon3.classList.toggle('b');
+    nav.classList.toggle('show');
+    //blue.classList.toggle('slide');
+});
